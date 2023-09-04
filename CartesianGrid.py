18c609ba375c1db9345e0bf2a7aeb18df60db088
Christian Singer <christian.singer@mail.schwarz>
2023-09-04 12:07:29 +0200

    - Center coordinate system aorund zero

--- CartesianGrid.py
+++ CartesianGrid.py
@@ -1,5 +1,5 @@
 from PySide6.QtWidgets import QGraphicsItem
-from PySide6.QtGui import QPainter, QPen
+from PySide6.QtGui import QPen
 from PySide6.QtCore import Qt, QRectF
 
 
@@ -11,21 +11,21 @@ class CartesianGrid(QGraphicsItem):
         self.cellSize = cellSize
 
     def boundingRect(self):
-        return QRectF(0, 0, self.width, self.height)
+        return QRectF(-self.width/2, -self.height/2, self.width, self.height)
 
     def paint(self, painter, option, widget):
-        left, right, top, bottom = 0, self.width, 0, self.height
-        for x in range(left, right, self.cellSize):
+        left, right, top, bottom = -self.width/2, self.width/2, -self.height/2, self.height/2
+        for x in range(int(left), int(right), self.cellSize):
             painter.setPen(Qt.lightGray)
             painter.drawLine(x, top, x, bottom)
 
-        for y in range(top, bottom, self.cellSize):
+        for y in range(int(top), int(bottom), self.cellSize):
             painter.setPen(Qt.lightGray)
             painter.drawLine(left, y, right, y)
 
         axesPen = QPen(Qt.black)
         axesPen.setWidth(1)
         painter.setPen(axesPen)
-        painter.drawLine(left, self.height/2, right, self.height/2)  # X-axis
-        painter.drawLine(self.width/2, top, self.width/2, bottom)    # Y-axis
+        painter.drawLine(left, 0, right, 0)
+        painter.drawLine(0, top, 0, bottom)
 
